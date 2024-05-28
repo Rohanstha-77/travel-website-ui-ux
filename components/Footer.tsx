26c238804ad322379d19298e22a8ac845dc08c9a
Rohan <nikeshshrestha405@gmail.com>
2024-05-28 23:55:16 +0545

    fixing error

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FOOTER_CONTACT_INFO,FOOTER_LINKS, SOCIALS } from '../constants'
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from '../constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -9,15 +9,15 @@ const Footer = () => {
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-            <Image src="hilink-logo.svg" alt="logo" width={74} height={29}/>
+            <Image src="/hilink-logo.svg" alt="Hilink logo" width={74} height={29}/>
           </Link>
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
-            {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
+            {FOOTER_LINKS.map((columns, index) => (
+              <FooterColumn key={index} title={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
-                  {columns.links.map((link) => (
-                    <Link href="/" key={link}>
+                  {columns.links.map((link, linkIndex) => (
+                    <Link href="/" key={linkIndex}>
                       {link}
                     </Link>
                   ))}
@@ -27,10 +27,10 @@ const Footer = () => {
 
             <div className="flex flex-col gap-5">
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
-                {FOOTER_CONTACT_INFO.links.map((link) => (
+                {FOOTER_CONTACT_INFO.links.map((link, index) => (
                   <Link
                     href="/"
-                    key={link.label}
+                    key={index}
                     className="flex gap-4 md:flex-col lg:flex-row"
                   >
                     <p className="whitespace-nowrap">
@@ -47,9 +47,9 @@ const Footer = () => {
             <div className="flex flex-col gap-5">
               <FooterColumn title={SOCIALS.title}>
                 <ul className="regular-14 flex gap-4 text-gray-30">
-                  {SOCIALS.links.map((link) => (
-                    <Link href="/" key={link}>
-                      <Image src={link} alt="logo" width={24} height={24} />
+                  {SOCIALS.links.map((link, index) => (
+                    <Link href="/" key={index}>
+                      <Image src={link} alt={`${link} logo`} width={24} height={24} />
                     </Link>
                   ))}
                 </ul>
